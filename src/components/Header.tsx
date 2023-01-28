358888b1dab2db391f3fcd12e695d1ac03072986
Faukkirik <kote_33419@mai.ru>
2023-01-28 20:38:12 +0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+            <header className={'header'}>
+                <img
+                    src="https://avatars.mds.yandex.net/i?id=a50def483893c1f28f909c2d182a1599de4b5a80-5099198-images-thumbs&n=13"
+                    alt=""/>
+            </header>
+    );
+};
+
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/605e1b31691245.565cd01085b21.jpg"
+                     alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
