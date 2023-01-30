370148e3bca16294d0bbc5b010c026c536159f3f
Faukkirik <kote_33419@mai.ru>
2023-01-30 15:36:55 +0300

    5 commit - 18 track Dialogs

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img className={s.lina} src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/605e1b31691245.565cd01085b21.jpg"
                      alt=""/>
